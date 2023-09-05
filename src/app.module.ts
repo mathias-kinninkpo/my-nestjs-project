56124a44693cafc7242993dd443669a9376608a5
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-09-05 18:05:19 +0100

    modification

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { PrismaService } from './prisma/prisma.service';
+import { UsersModule } from './users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './auth/jwt.strategy';
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [ArticlesModule, UsersModule,
+      PassportModule,
+      JwtModule.register({
+        secret: 'codesecrete', 
+        signOptions: { expiresIn: '120h' }, 
+      }),
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, JwtStrategy],
 })
 export class AppModule {}
