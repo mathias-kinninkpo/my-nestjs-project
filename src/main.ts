edda5e18d60e132b4bba2fdef165487a115c265f
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-09-06 17:04:27 +0100

    modification et ajout de fonctionnalité

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe());
+  const server = express(); // Créez une instance d'Express
+
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server), // Utilisez l'adaptateur Express pour Nest.js
+  );
+
+  console.log(path.join(__dirname, '..'))
+  server.use('..', express.static(path.join(__dirname, '..')));
+
   await app.listen(3000);
 }
 bootstrap();
