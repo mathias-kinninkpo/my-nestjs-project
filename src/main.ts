32fe5a2c81b62c84b9d31ae265fda21e28656ca6
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-09-07 19:25:15 +0100

    configuration de l'application pour swagger

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,32 @@ import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import * as path from 'path';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
-  const server = express(); // Créez une instance d'Express
+  const server = express(); 
 
   const app = await NestFactory.create(
     AppModule,
-    new ExpressAdapter(server), // Utilisez l'adaptateur Express pour Nest.js
+    new ExpressAdapter(server), 
   );
 
   console.log(path.join(__dirname, '..'))
-  server.use('..', express.static(path.join(__dirname, '..')));
-
+  app.use('.', express.static(path.join(__dirname, '..')));
+  const config = new DocumentBuilder()
+  .setTitle('Articles API')
+  .setDescription('API de gestion des articles')
+  .setVersion('1.0')
+  .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
+
+
+
